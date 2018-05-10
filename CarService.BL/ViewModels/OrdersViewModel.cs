1492a8cc68ca6b4a215ff201fceb42efece16e2b
Dmitry Vasiliev <ceh4top@gmail.com>
2018-05-11 02:47:11 +0300

    Удаление неиспользуемые директивы using во всех файлах

--- CarService.BL/ViewModels/OrdersViewModel.cs
+++ CarService.BL/ViewModels/OrdersViewModel.cs
@@ -1,10 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.ComponentModel;
-using System.Runtime.CompilerServices;
 using System.Collections.ObjectModel;
 using CarService.DAL.Models;
 using CarService.DAL.Repositories;
